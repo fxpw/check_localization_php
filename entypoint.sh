983dd73eb358ea5518b46efbe4a3cc07867b273b
fxpw <fxpw@outlook.com>
2024-08-19 12:13:20 +0300

    f

--- entypoint.sh
+++ entypoint.sh
@@ -5,14 +5,13 @@ contains_russian() {
     echo "$1" | grep -q '[а-яА-Я]'
 }
 
-# Получаем список измененных файлов
 changed_files=$(git diff --name-only "$GITHUB_REF" "$GITHUB_SHA")
 
 for filename in $changed_files; do
-    if [[ "$filename" == *.blade.php ]]; then  # Правильная проверка на окончание файла
+    if [[ "$filename" == *.blade.php ]]; then
         while IFS= read -r line; do
             if contains_russian "$line"; then
-                echo "Файл \"$filename\": строка \"$line\" - требует локализации."
+                echo "File \"$filename\": Line \"$line\" - need locale."
             fi
         done < "$filename"
     fi
