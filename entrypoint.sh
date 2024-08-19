04cd1034b41b10bd5a39942ac99bf014c57245e1
fxpw <fxpw@outlook.com>
2024-08-19 13:17:57 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,18 +9,25 @@ git config --global credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com" > .git/credentials
 git config --global --add safe.directory /github/workspace
 
+
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+    BASE_BRANCH="${GITHUB_BASE_REF}"
+else
+    BASE_BRANCH="main"
+fi
+
 pr_number="${GITHUB_REF##*/}"
 git fetch origin
-git fetch origin pull/"$pr_number"/merge:refs/remotes/origin/pr"$pr_number"
-changed_files=$(git diff --name-only refs/remotes/origin/pr"$pr_number" "$GITHUB_SHA")
 
-# changed_files=$(git diff --name-only FETCH_HEAD "$GITHUB_SHA")
+changed_files=$(git diff --name-only "origin/$BASE_BRANCH" "$GITHUB_SHA")
+
 localization_needed=false
 for filename in $changed_files; do
     if [[ "$filename" == *.blade.php ]]; then
         while IFS= read -r line; do
             if contains_russian "$line"; then
-                echo "File \"$filename\": Line \"$line\" - need locale."
+				line_number=$(echo "$line" | awk '{ print NR }')
+                echo "File ${filename}:${line_number} Line \"$line\""
 				localization_needed=true
             fi
         done < "$filename"
