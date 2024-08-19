214c12ffce9d943acd820c8fc809f1e132f0b3ca
fxpw <fxpw@outlook.com>
2024-08-19 13:06:16 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,19 @@ git fetch origin pull/"$pr_number"/merge:refs/remotes/origin/pr"$pr_number"
 changed_files=$(git diff --name-only refs/remotes/origin/pr"$pr_number" "$GITHUB_SHA")
 
 # changed_files=$(git diff --name-only FETCH_HEAD "$GITHUB_SHA")
-
+localization_needed=false
 for filename in $changed_files; do
     if [[ "$filename" == *.blade.php ]]; then
         while IFS= read -r line; do
             if contains_russian "$line"; then
                 echo "File \"$filename\": Line \"$line\" - need locale."
+				localization_needed=true
             fi
         done < "$filename"
     fi
 done
+
+if $localization_needed; then
+    echo "Find files for localization."
+    exit 1
+fi
